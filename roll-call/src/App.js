8c1e04bbb7e32e5c889d4c9b57e6deffd5bd2577
Vikas Shukla <shuk.vk@gmail.com>
2020-02-29 12:55:03 -0800

    adds landing page

--- roll-call/src/App.js
+++ roll-call/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <section class="banner_part">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 col-xl-6">
+              <div class="banner_text">
+                <div class="banner_text_iner">
+                  <h5>Every child yearns to learn</h5>
+                  <h1>Making Your Childs
+                                World Better</h1>
+                  <p>Replenish seasons may male hath fruit beast were seas saw you arrie said man beast whales
+                      his void unto last session for bite. Set have great you'll male grass yielding yielding
+                                man</p>
+                  <a href="#" class="btn_1">View Course </a>
+                  <a href="#" class="btn_2">Get Started </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
